0b66247d93621e5f213f8052e2423bdf821db630
Graham Briggs <grahambriggs@gmail.com>
2026-08-22 23:47:40 +0900

    std::print c++23

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 
 #include <cmath>
 #include <memory>
-#include <iostream>
+#include <print>
 #include <vector>
 
 #include "asset_loader.h"
@@ -17,13 +17,13 @@
 #include "utility.h"
 #include "video.h"
 
-//#include "image_asset_resource.h"
+#include "collision.h"
 
 int main(int argc, char* args[]) {
 
 
 	auto s = file_read("./test_map.txt");
-	std::cout << s << std::endl;
+	std::println("{}", s);
 	auto v = file_read_lines(s);
 	auto tri = read_tile_file("./test_map.txt");
 
@@ -44,34 +44,41 @@ int main(int argc, char* args[]) {
 	std::string ts_path = "./images/smb_tilesheet.bmp";
 	auto iar = std::make_unique<ImageAssetResource>(video.get(), ts_path);
 
-	/// test here
+
+	// Tile tile(iar.get(), 256, 256, 32, 32);
+	// tile.setImageClip(SDL_Rect{0 ,0,32,32});
+
+	// Tile collision_tile(iar.get(), 128, 256, 32, 32);
+	// collision_tile.setImageClip(SDL_Rect{64,0,32,32});
+	// collision_tile.setCollidable(true);
 
 
-
-	//auto rti = tri[0];
-	//Tile test_read_tile(iar.get(), rti.bb_x, rti.bb_y, rti.bb_w, rti.bb_h);
-	////
-
-
-	Tile tile(iar.get(), 256, 256, 32, 32);
-	tile.setImageClip(SDL_Rect{0 ,0,32,32});
-
-	Tile collision_tile(iar.get(), 128, 256, 32, 32);
-	collision_tile.setImageClip(SDL_Rect{64,0,32,32});
-	collision_tile.setCollidable(true);
-
-
-	std::vector<Tile> tiles {tile, collision_tile };
+	//std::vector<Tile> tiles {tile, collision_tile };
+	std::vector<Tile> tiles;
 	//tiles.push_back(test_read_tile);
 	for (auto rti : tri) {
-		Tile test_read_tile(iar.get(), rti.bb_x, rti.bb_y, rti.bb_w, rti.bb_h);
+		Tile temp(iar.get(), rti.bb_x, rti.bb_y, rti.bb_w, rti.bb_h);
 		SDL_Rect img_clip{rti.tilesheet_x, rti.tilesheet_y, 32, 32};
-		test_read_tile.setImageClip(img_clip);
+		temp.setImageClip(img_clip);
+		
 		if (rti.is_collide) {
-			test_read_tile.setCollidable(true);
-			std::cout << "setColliadable(true);" << std::endl;
+			temp.setCollidable(true);
+			std::println("setColliadable(true);");
 		}
-		tiles.push_back(test_read_tile);
+		else {
+			temp.setCollidable(false);
+		}
+		tiles.push_back(temp);
+	}
+
+	for (auto t : tiles) {
+		if (t.getIsCollidable()) {
+			std::println("Collidable");
+			
+		}
+		else {
+			std::println("not collidable");
+		}
 	}
 
 	while (device->run()) {
@@ -87,8 +94,19 @@ int main(int argc, char* args[]) {
 		//current_level->level_player_logic(player.get());
 
 		for (auto t : tiles) {
+
+			const SDL_Rect& pbb = player->getBoundingBox();
+			const SDL_Rect& tbb = t.getBoundingBox();
+
+			//ayer->tile_collide(t);
+			// if (aabb_collision(pbb, tbb)) {
+			// 	if (t.getIsCollidable()) {
+			// 		std::cout << "collision" << std::endl;
+			// 	}
+			// }
+			
 			if (t.getIsCollidable()) {
-				player->tile_collide(tile);
+				player->tile_collide(t);
 			}
 		}
 		
@@ -98,7 +116,7 @@ int main(int argc, char* args[]) {
 		//current_level->render_level(video.get());
 		video->setDrawColor(0, 0, 255, 255);
 		video->setDrawColor(42, 69, 1, 255);
-		video->drawRectangle(100, 100, 64, 64);
+		video->drawRectangle(800, 800, 64, 64);
 		//video->drawSprite(&anim_tile);
 		
 		for (auto t : tiles) {
